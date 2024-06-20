3705766fe6f14d91c04a17b0c870ed627cfb6c16
Tiago Pereira <tfgp@ua.pt>
2024-06-20 16:27:50 +0100

    Preprocess to clean original dataset

--- data/preprocess.py
+++ data/preprocess.py
@@ -6,7 +6,7 @@ data = pd.read_csv('../Datasets/combined_data.csv')
 X = data.iloc[:,1]
 y = data.iloc[:,0]
 num_examples = X.shape[0]
-file = open("../Datasets/cleaned_combined_data.csv", "w+")
+file = open("../Datasets/cleaned_combined_data.csv", "w+",  encoding='utf-8')
 
 def detect_language(text):
     try:
@@ -18,14 +18,8 @@ multiline = re.compile('.*\n.*')
 for index in range(num_examples):
     lang = detect_language(X[index])
     if lang == 'en':
-        file.write(str(y[index]))
-        file.write(",")
-        if multiline.match(X[index]): #Multiline text
-            file.write("\"")
-            file.write(X[index])
-            file.write("\"")
-        else:
-            file.write(X[index])
-        file.write("\n")
+            label = str(y[index])
+            text = X[index].replace('"', '""')  # Escape double quotes within the text
+            file.write(f'{label},"{text}"\n')
 
 file.close()
